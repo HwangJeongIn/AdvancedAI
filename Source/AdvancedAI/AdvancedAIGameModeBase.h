1931f28955f2708516a60005bdc845260c664a20
HwangJeongIn <36975010+HwangJeongIn@users.noreply.github.com>
2022-09-14 19:59:57 +0900

    빈 프로젝트 추가

---
+++ Source/AdvancedAI/AdvancedAIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdvancedAIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADVANCEDAI_API AAdvancedAIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
